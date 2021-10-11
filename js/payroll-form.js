fdc030e5f135fcb5ccee0a56a5ca377e8f004e57
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-11 23:51:51 +0530

    ADDED: event listener set to display salary range value

---
+++ js/payroll-form.js
@@ -0,0 +1,5 @@
+var salaryOutput = document.querySelector(".salary-output");
+var salary = document.querySelector("#salary");
+salary.addEventListener("input", function () {
+  salaryOutput.textContent = salary.value;
+});
