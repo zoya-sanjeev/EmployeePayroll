9dde1194d48625bb72332beaf5c45d33e91a5a64
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-12 23:48:09 +0530

    ADDED: function to reset the form when reset button clicked

--- js/payroll-form.js
+++ js/payroll-form.js
@@ -44,6 +44,7 @@ window.addEventListener('DOMContentLoaded', (event)=>{
 const save = () => {
   try{
     let employeePayrollData = createEmployeePayroll();
+    createAndUpdateStorage(employeePayrollData);
   }catch(e){
     return;
   }
@@ -91,6 +92,32 @@ const getInputValueById =(id) =>{
   let value =document.querySelector(id).value;
   return value;
 }
+const resetForm = () => {
+  setValue('#name','');
+  unsetSelectedValues('[name=profile]');
+  unsetSelectedValues('[name=gender]');
+  unsetSelectedValues('[name=department]');
+  setValue('#salary','');
+  setValue('#notes','');
+  setValue('#day','1');
+  setValue('#month','January');
+  setValue('#year','2020');
+}
 
+const unsetSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  allItems.forEach(item =>{
+      item.checked = false;
+  });
+}
+const setTextValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.textContent = value;
+}
+
+const setValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.value = value;
+}
 
 
