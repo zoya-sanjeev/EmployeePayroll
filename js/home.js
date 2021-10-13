f5f64c071d825e9bf7a81ce0e3cc5cba8144e600
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-14 00:40:16 +0530

    ADDED:displaying details using template literals

--- js/home.js
+++ js/home.js
@@ -3,33 +3,31 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHtml = () => {
-    const innerHtml = `
+
+    const headerHtml = `
+      <th></th>
+      <th>Name</th>
+      <th>Gender</th>
+      <th>Department</th>
+      <th>Salary</th>
+      <th>Start Date</th>
+      <th>Actions</th>`;
+  
+    const innerHtml = `${headerHtml}
       <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Gender</th>
-              <th>Department</th>
-              <th>Salary</th>
-              <th>Start Date</th>
-              <th>Actions</th>
-            </tr>
-            <tr>
-              <td>
-                <img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png"/>
-              </td>
-              <td>Narayan Mahadevan</td>
-              <td>Male</td>
-              <td>
-                <div class="dept-label">HR</div>
-                <div class="dept-label">Finance</div>
-              </td>
-              <td>3000000</td>
-              <td>1 Nov 2020</td>
-              <td>
-                <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg"/>
-                <img id="1" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg"/>
-              </td>
-            </tr>
-      `;
-    document.querySelector("#table-display").innerHTML = innerHtml;
-  };
+        <td><img class="profile" src="../assets/profile-images/profile_img-1.png" alt="profile_img-1"></td>
+        <td>Ashika</td>
+        <td>Female</td>
+        <td>
+          <div class="dept-label">HR</div>
+          <div class="dept-label">Finance</div>
+        </td>
+        <td>3000000</td>
+        <td>12 Oct 2021</td>
+        <td>
+          <img src="../assets/icons/delete.svg" alt="delete" id="1" onclick="remove(this)">
+          <img src="../assets/icons/edit.svg" alt="edit" id="1" onclick="update(this)">
+        </td>
+      </tr>`;
+    document.querySelector('#table-display').innerHTML = innerHtml;
+  }
