333b2d7ca1ffae3949df4b6e31d102c37fa056b3
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-18 21:53:13 +0530

    MODIFIED: remove function to remove employee from server

--- js/home.js
+++ js/home.js
@@ -90,17 +90,27 @@ const getDeptHtml = (departmentList) => {
 };
 
 const remove = (node) => {
-    console.log(node);
-    console.log(employeePayrollList);
     let empPayrollData = employeePayrollList.find(empData => empData.id == node.id);
     if (!empPayrollData) return;
     const index = employeePayrollList
       .map(empData => empData.id)
       .indexOf(empPayrollData.id);
     employeePayrollList.splice(index, 1);
-    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
-    document.querySelector(".emp-count").textContent = employeePayrollList.length;
-    createInnerHtml();
+    if(site_properties.use_local_storage.match("true")){
+      localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
+      document.querySelector(".emp-count").textContent = employeePayrollList.length;
+      createInnerHtml();
+    }else{
+      const deleteURL =site_properties.server_url + empPayrollData.id.toString();
+      makeServiceCall("DELETE", deleteURL, false)
+        .then(responseText =>{
+          document.querySelector(".emp-count").textContent = employeePayrollList.length;
+          createInnerHtml();
+        })
+        .catch(error =>{
+          console.log("DELETE Error Status: " + JSON.stringify(error));
+        });
+    }
   };
 
 const update = (node) => {
