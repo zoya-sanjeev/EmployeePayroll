4daad6c5e776f6416697e84b34c271840c4034a8
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-18 21:41:56 +0530

    ADDED: function to add employee to using post method

--- js/payroll-form.js
+++ js/payroll-form.js
@@ -52,14 +52,35 @@ const save = (event) => {
   event.stopPropagation();
   try {
     setEmployeePayrollObject();
-    createAndUpdateStorage();
-    resetForm();
-    window.location.replace(site_properties.home_page);
+    if(site_properties.use_local_storage.match("true")){
+      createAndUpdateStorage();
+      resetForm();
+      window.location.replace(site_properties.home_page);
+    }else{
+      createOrUpdateEmployeePayroll();
+    }
   } catch (e) {
     console.error(e);
     return;
   }
 };
+
+const createOrUpdateEmployeePayroll = () => {
+  let postURL = site_properties.server_url;
+  let methodCall = "POST";
+  if(isUpdate){
+    methodCall = "PUT";
+    postURL = postURL + employeePayrollObj.id.toString();
+  }
+  makeServiceCall(methodCall, postURL, true, employeePayrollObj)
+    .then(responseText =>{
+      resetForm();
+      window.location.replace(site_properties.home_page);
+    })
+    .catch(error => {
+      throw error;
+    });
+}
 
 const setEmployeePayrollObject = () => {
   if(!isUpdate && site_properties.use_local_storage.match("true")){
