21d13aa267852ffb8558bafad05e09a4966556fc
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-18 19:13:15 +0530

    ADDED: function to validate name and start date added to utility.js

--- js/utility.js
+++ js/utility.js
@@ -3,3 +3,16 @@ const stringifyDate = (date) => {
     const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
     return newDate;
   }
+
+const checkName = (name) => {
+  let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+  if(!nameRegex.test(name)) throw 'Name is Incorrect!';
+}
+
+const checkStartDate = (startDate) => {
+  let now = new Date();
+  if(startDate > now) throw 'Start Date is a future Date!';
+  var diff = Math.abs(now.getTime - startDate.getTime());
+  if(diff /(1000 * 60 * 60 * 24) > 30)
+    throw 'Start Date is beyond 30 Days';
+}
