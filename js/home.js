b3fd09e98459fabc10dfa0d5b23e3115ee050069
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-18 21:16:35 +0530

    REFACTORED: home page to get employee payroll data from server and storage

--- js/home.js
+++ js/home.js
@@ -38,7 +38,27 @@ const createInnerHtml = () => {
     "<th>Salary</th><th>Start Date</th><th>Actions</th></tr>";
   if (employeePayrollList.length == 0) return;
   let innerHtml = `${headerHtml}`;
-  for (const empPayrollData of employeePayrollList) {
+  if(site_properties.use_local_storage.match("false"))
+  for (const empPayrollData of employeePayrollList) { 
+    innerHtml = `${innerHtml}
+        <tr>
+        <td><img class="profile" alt="" src="${
+          empPayrollData._profilePic
+        }"></td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}
+        <td>${empPayrollData._salary}</td>
+        <td>${stringifyDate(empPayrollData._startDate)}</td>
+        <td>
+        <img id="${empPayrollData.id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+        <img id="${empPayrollData.id}" alt="edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+        </td>
+        </tr>
+        `;
+  }
+  else
+  for (const empPayrollData of employeePayrollList) { 
     innerHtml = `${innerHtml}
         <tr>
         <td><img class="profile" alt="" src="${
@@ -56,6 +76,7 @@ const createInnerHtml = () => {
         </tr>
         `;
   }
+
   document.querySelector("#table-display").innerHTML = innerHtml;
 };
 
